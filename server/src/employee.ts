634b8e49423c16bb1437889f050fce32a4632fc6
d22003660 <shock7170@gmail.com>
2024-12-16 11:26:34 +0100

    description

--- server/src/employee.ts
+++ server/src/employee.ts
@@ -4,6 +4,8 @@ export interface Employee {
     name: string;
     surname: string;
     salary: Number;
+    description: string;
+    datecreate: Date;
     position: string;
     level: "junior" | "mid" | "senior";
     _id?: mongodb.ObjectId;
