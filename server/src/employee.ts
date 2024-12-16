f71feb032b7c9c1d3ebd0f17f83b290f82ac5bf6
d22003660 <shock7170@gmail.com>
2024-12-16 11:17:39 +0100

    ajout salary and surname

--- server/src/employee.ts
+++ server/src/employee.ts
@@ -2,6 +2,8 @@ import * as mongodb from "mongodb";
 
 export interface Employee {
     name: string;
+    surname: string;
+    salary: Number;
     position: string;
     level: "junior" | "mid" | "senior";
     _id?: mongodb.ObjectId;
