222422a7f0ad85ddd281905d17eb2a4ffc282618
BAMAS-Mathias <byonyxmc@gmail.com>
2024-12-16 11:55:28 +0100

    ajout suppression

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -2,8 +2,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import Employee from "./Employee";
 import "./employee.css";
+import UserDetails from "./UserDetails";
 const Dashboard = () => {
   const [userList, setUserList] = useState([]);
+  const [userDetailsId, setUserDetailsId] = useState(null);
 
   useEffect(() => {
     axios
@@ -16,14 +18,37 @@ const Dashboard = () => {
       });
   }, []);
 
+  const deleteUser = (id) => {
+    axios
+      .delete(import.meta.env.VITE_API_URL + "/employees/" + id)
+      .then((response) => {
+        setUserList(userList.filter((user) => user._id !== id));
+        setUserDetailsId(null);
+      })
+      .catch((error) => {
+        console.error("Error deleting user: ", error);
+      });
+  };
+
   return (
     <>
       <input type="text" className="opacity-100 h-10 min-w-[450px] mb-5" />
       <div className="employee-grid">
         {userList.map((employee, index) => (
-          <Employee key={index} employee={employee} />
+          <Employee
+            key={index}
+            employee={employee}
+            onclick={() => setUserDetailsId(employee._id)}
+          />
         ))}
       </div>
+      {userDetailsId != null && (
+        <UserDetails
+          _id={userDetailsId}
+          closeMenu={() => setUserDetailsId(null)}
+          deleteUser={deleteUser}
+        />
+      )}
     </>
   );
 };
--- client/src/pages/dashboard/Employee.jsx
+++ client/src/pages/dashboard/Employee.jsx
@@ -1,11 +1,14 @@
-const Employee = ({ employee }) => {
+const Employee = ({ employee, onclick }) => {
   const levelColor = {
     mid: "bg-yellow-500",
     junior: "bg-green-500",
     senior: "bg-red-500",
   };
   return (
-    <div className="flex size-64 bg-white bg-opacity-10 rounded h-[100px] w-[450px] flex-col py-4 px-4 ">
+    <div
+      className="flex size-64 bg-white bg-opacity-10 rounded h-[100px] w-[450px] flex-col py-4 px-4 cursor-pointer hover:bg-opacity-20   transition-all "
+      onClick={onclick}
+    >
       <div className="flex justify-between">
         <div className="">
           <p className="font-bold text-xl">{employee.name}</p>
